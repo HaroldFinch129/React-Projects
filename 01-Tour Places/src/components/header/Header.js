aac8c670823a6cfc4291ca078b1bbfedaf0c831d
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-14 16:00:32 +0300

    Add files via upload

---
+++ 01-Tour Places/src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return ( <
+        div className = "big-title" >
+        <
+        h1 > POPULAR TOUR PLACES < /h1>
+
+        <
+        /div>
+    )
+}
+
+export default Header;

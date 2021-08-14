2869eb1c97a5a8aa3d563fd1a1765c2c6611ac6e
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-14 17:50:24 +0300

    Add files via upload

--- 01-Tour Places/src/components/cards/Card.js
+++ 01-Tour Places/src/components/cards/Card.js
@@ -1,21 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { data } from "../../helper/data";
+import "./Card.css";
 
-
-function Card(probs) {
+function Card() {
+    const [showImage, setShowImage] = useState(true);
     return ( <
-        div >
-        <
-        h2 > CardName < /h2> <
-        img src = "https://picsum.photos/300/200" > < /img>
-
-        <
+        div className = "card-container"
+        onClick = {
+            () => setShowImage(!showImage) } > {
+            showImage ? ( <
+                > {
+                    data.map((card) => {
+                        return ( <
+                            div className = "cards"
+                            key = { card.id } >
+                            <
+                            img src = { card.image }
+                            alt = { card.title }
+                            /> <
+                            /div>
+                        );
+                    })
+                } <
+                />
+            ) : ( <
+                > {
+                    data.map((card) => {
+                        return ( <
+                            div className = "cards"
+                            key = { card.id } >
+                            <
+                            div className = "title" >
+                            <
+                            h2 > { card.title } < /h2> <
+                            /div> <
+                            img src = { card.image }
+                            alt = { card.title }
+                            /> <
+                            div className = "card-over" >
+                            <
+                            p > { card.desc } < /p> <
+                            /div> <
+                            /div>
+                        );
+                    })
+                } <
+                />
+            )
+        } <
         /div>
-        //     <
-        // //     div >
-        // //     <
-        // //     h2 > CardName < /h2> <
-        // //     img src = "https://picsum.photos/300/200" > < /img> <
-        // //     /div>
-    )
-
+    );
 }
+export default Card;
+
+
+// function Card(props) {
+//     return (
+//         <div className="card-container">
+//           {data.map((card) => {
+//             return (
+//               <div className="cards">
+//                 <div className="title">
+//                   <h2>{card.title}</h2>
+//                 </div>
+//                 <img src={card.image} alt={card.title} />
+//                 <div className= "card-over">
+//                   <p>{card.desc}</p>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </div>
+//       );
+//     }
+//     export default Card;

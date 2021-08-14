02d387ade5c161d44cb80b95cc862fb0c6b3c2a6
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-14 15:58:09 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+const App = () => {
+    return ( <
+        div className = "App" >
+        <
+        Navbar / >
+        <
+        Header / >
+        <
+        /div>
+    )
+}
+
+export default App;
---
+++ src/components/cards/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Card(probs) {
+    return ( <
+        div >
+        <
+        h2 > CardName < /h2> <
+        img src = "https://picsum.photos/300/200" > < /img>
+
+        <
+        /div>
+        //     <
+        // //     div >
+        // //     <
+        // //     h2 > CardName < /h2> <
+        // //     img src = "https://picsum.photos/300/200" > < /img> <
+        // //     /div>
+    )
+
+}

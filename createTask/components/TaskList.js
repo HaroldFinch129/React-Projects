dcb41b49a6897de3e48b0aacf0abe5d437e8ce4a
HaroldFinch129 <81425579+HaroldFinch129@users.noreply.github.com>
2021-08-26 22:58:03 +0300

    Add files via upload

---
+++ createTask/components/TaskList.js
@@ -0,0 +1,16 @@
+import { TaskItem } from "./TaskItem";
+
+export default function TaskList({ tasks, onDelete }) {
+
+    return ( <
+        div > {
+            tasks.map((task) => ( <
+                TaskItem key = { task.id }
+                task = { task }
+                onDelete = { onDelete }
+                />
+            ))
+        } <
+        /div>
+    );
+}
